2b1cfa9a768c98cb9c42b9a8f89da2e535374b56
Mason Balbera <mhbalbera@gmail.com>
2019-07-26 17:33:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
+ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
   puts status
 end
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ 
   puts phrase
 end
 
